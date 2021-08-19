ece58eb508e024bca63c10bcc83df1ca7cbbdcb1
Raúl Crisóstomo <raul.crisostomo@mercadolibre.com.mx>
2021-08-19 10:55:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,1 @@
 <?php require_once("Index.html")?>
-<?php require_once("Menu.html")?>
-<?php require_once("Mesas.html")?>
-<?php require_once("QRC.html")?>
-<?php require_once("Registradora1.html")?>
-<?php require_once("Registradora2.html")?>
-<?php require_once("Registradora3.html")?>
